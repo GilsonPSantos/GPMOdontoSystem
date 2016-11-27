4caee8003d6f6a31a3beae1bc0d74016c7482114
GilsonPSantos <gilsonsantosti@gmail.com>
2016-11-27 11:13:02 -0200

    fdfdfdf

--- GPMOdontoSystem/src/br/com/gpmodontosystem/manager/paciente/PacienteBean.java
+++ GPMOdontoSystem/src/br/com/gpmodontosystem/manager/paciente/PacienteBean.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.gpmodontosystem.model.Paciente;
 import br.com.gpmodontosystem.persistence.pacientedao.IPacienteDao;
 import br.com.gpmodontosystem.persistence.pacientedao.PacienteDaoImp;
+import br.com.gpmodontosystem.service.paciente.IPacienteService;
+import br.com.gpmodontosystem.service.paciente.PacienteServiceImp;
+import br.com.gpmodontosystem.type.TypeSexo;
 
 @ManagedBean(name="mbPaciente")
 @RequestScoped
@@ -20,14 +25,59 @@ public class PacienteBean implements Serializable {
 	private Paciente paciente;
 	private List<Paciente> listaPaciente;
 	
-	private IPacienteDao pacienteDao;
+	private IPacienteService pacienteService;
+	
+	private TypeSexo tipoSexo;
 	
 	@PostConstruct
 	public void init(){
 		paciente = new Paciente();
 		listaPaciente = new ArrayList<Paciente>();
-		pacienteDao = new PacienteDaoImp();
+		pacienteService = new PacienteServiceImp();
+		
+		//dados fack para testes.
+		paciente.setNomePessoa("Gilson Santos");
+		paciente.setCpf("010212112121");
+		paciente.setSexo(TypeSexo.M);
+		paciente.setIdentidade("123456");
+		
 	}
+	
+	public void cadastrar(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		try {
+			pacienteService.inserir(paciente);
+			fc.addMessage("formCadConsPaciente", new FacesMessage("Paciente gravado com sucesso."));
+		} catch (Exception e) {
+			fc.addMessage("formCadConsPaciente", new FacesMessage("Ocorreu um erro interno ao salvar o Paciente."));
+			e.printStackTrace();
+		}
+	}
+	
+	public void consultar(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		
+		try {
+			paciente = pacienteService.consultarPeloId(paciente);
+		} catch (Exception e) {
+			fc.addMessage("formCadConsPaciente", new FacesMessage("Ocorreu um erro interno ao consultar o Paciente."));
+			e.printStackTrace();
+		}
+	}
+	
+	public void excluir(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		
+		
+	}
+	
+	public String cancelarCadastro(){
+		
+		return null;
+	}
+	
+	
+	
 	
 	
 
@@ -47,16 +97,16 @@ public class PacienteBean implements Serializable {
 		this.listaPaciente = listaPaciente;
 	}
 
-	public IPacienteDao getPacienteDao() {
-		return pacienteDao;
-	}
-
-	public void setPacienteDao(IPacienteDao pacienteDao) {
-		this.pacienteDao = pacienteDao;
-	}
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public TypeSexo getTipoSexo() {
+		return tipoSexo;
+	}
+
+	public void setTipoSexo(TypeSexo tipoSexo) {
+		this.tipoSexo = tipoSexo;
 	}
 	
 	
